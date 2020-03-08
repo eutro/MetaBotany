f5062d12b8d9540bea372c90b05d8be7e78ba05f
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-03-08 18:03:25 +0000

    Update Frame Tinkerer rendering.

--- src/main/java/eutros/botaniapp/client/render/RenderTileFrameTinkerer.java
+++ src/main/java/eutros/botaniapp/client/render/RenderTileFrameTinkerer.java
@@ -1,6 +1,7 @@
 package eutros.botaniapp.client.render;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import eutros.botaniapp.client.core.handler.ClientTickHandler;
 import eutros.botaniapp.common.block.tinkerer.tile.TileFrameTinkerer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
@@ -11,6 +12,8 @@ import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Random;
+
 public class RenderTileFrameTinkerer extends TileEntityRenderer<TileFrameTinkerer> {
 
 	public RenderTileFrameTinkerer(TileEntityRendererDispatcher p_i226006_1_) {
@@ -20,12 +23,14 @@ public class RenderTileFrameTinkerer extends TileEntityRenderer<TileFrameTinkere
 	@Override
 	public void render(TileFrameTinkerer plate, float v, MatrixStack ms, @NotNull IRenderTypeBuffer buffers, int light, int overlay) {
 		ms.push();
-		ms.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(90));
-		ms.translate(1.0F, -0.125F, -0.21875F);
 		ItemStack stack = plate.getItemHandler().getStackInSlot(0);
 		if(!stack.isEmpty()) {
-			ms.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180));
-			ms.translate(0.5F, 0.5F, 0);
+			Random rand = new Random(plate.getPos().hashCode());
+			float angle = (ClientTickHandler.total + 360F * rand.nextFloat()) * 2;
+			int period = 10;
+			float height = (float) Math.sin(ClientTickHandler.total / period + Math.PI * rand.nextFloat());
+			ms.translate(0.5F, 0.5F + height * 0.125, 0.5F);
+			ms.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(angle));
 			Minecraft.getInstance().getItemRenderer().renderItem(stack, ItemCameraTransforms.TransformType.GROUND, light, overlay, ms, buffers);
 		}
 		ms.pop();
