b21dd80fc5883ece703994d30334721f6397db82
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-03-11 22:01:26 +0000

    Improved the Flat Preset.

--- src/main/java/eutros/botaniapp/common/core/hacks/HackFlatPreset.java
+++ src/main/java/eutros/botaniapp/common/core/hacks/HackFlatPreset.java
@@ -1,24 +1,18 @@
 package eutros.botaniapp.common.core.hacks;
 
 import eutros.botaniapp.common.utils.Reference;
-import joptsimple.internal.Reflection;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.screen.FlatPresetsScreen;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.util.IItemProvider;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.FlatLayerInfo;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import vazkii.botania.common.block.ModBlocks;
 import vazkii.botania.common.item.ModItems;
 
-import java.lang.reflect.Method;
 import java.util.Collections;
-import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID)
 public class HackFlatPreset {
@@ -34,18 +28,12 @@ public class HackFlatPreset {
     }
 
     static void addPreset() {
-        Method addPreset = ObfuscationReflectionHelper.findMethod(FlatPresetsScreen.class,
-                "addPreset",
-                String.class,
-                IItemProvider.class,
-                Biome.class,
-                List.class,
-                FlatLayerInfo[].class);
-        Reflection.invoke(addPreset,
+        FlatPresetsScreen.addPreset(
                 I18n.format("botaniapp.customize.preset.botanist_ready"),
                 ModItems.twigWand,
                 Biomes.PLAINS,
                 Collections.emptyList(),
-                new FlatLayerInfo[]{new FlatLayerInfo(52, ModBlocks.livingrockBrick), new FlatLayerInfo(3, Blocks.STONE), new FlatLayerInfo(1, Blocks.BEDROCK)});
+                new FlatLayerInfo(64, ModBlocks.livingrockBrick), new FlatLayerInfo(1, Blocks.BEDROCK)
+        );
     }
 }
