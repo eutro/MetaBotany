f65ded66e36de04da8cb25da67f933d05fa4f4d7
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-03-16 01:04:39 +0000

    Here's the annotation.

---
+++ src/main/java/eutros/botaniapp/api/internal/config/Configurable.java
@@ -0,0 +1,30 @@
+package eutros.botaniapp.api.internal.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+public @interface Configurable {
+
+    /**
+     * The path, not including the field name.
+     */
+    String[] path() default {};
+
+    /**
+     * A comment for this field.
+     */
+    String[] comment() default {};
+
+    /**
+     * A localisation key for the comment.
+     */
+    String translation() default "";
+
+    /**
+     * A static method to call when the field is updated.
+     * The method must be a member of the same class as the field,
+     * and must take the field's type as a single parameter.
+     */
+    String callback() default "";
+}
